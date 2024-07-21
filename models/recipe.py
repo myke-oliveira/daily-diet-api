87a235d2041b67deb546fb37cc00ff96db2cfc9e
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-07-21 23:26:38 +0000

    ✨ NEW FEATURE: creation of recipe

--- models/recipe.py
+++ models/recipe.py
@@ -1,12 +1,13 @@
 from database import db
 from sqlalchemy import func
 from .user import User
+from datetime import datetime
 
 class Recipe(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(180), nullable=False, default="")
-    date_time = db.Column(db.DateTime(), nullable=False)
+    date_time: datetime = db.Column(db.DateTime(), nullable=False)
     on_the_diet = db.Column(db.Boolean(), nullable=False)
     user_id = db.Column(db.Integer(), db.ForeignKey(User.id), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=func.current_timestamp())
@@ -18,6 +19,6 @@ class Recipe(db.Model):
             "id": self.id,
             "name": self.name,
             "description": self.description,
-            "date_time": self.date_time,
+            "date_time": self.date_time.isoformat(),
             "on_the_diet": self.on_the_diet,
         }
